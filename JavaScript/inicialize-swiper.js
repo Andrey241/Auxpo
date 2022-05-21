0c5bba26428f5de4beccba25ff3cdf07c68fa55e
Andrey241 <Andrey75.9626@gmail.com>
2022-05-22 01:18:09 +0300

    html

--- JavaScript/inicialize-swiper.js
+++ JavaScript/inicialize-swiper.js
@@ -1,30 +1,30 @@
-// const count = ["1", "2", "3"];
-// const art = ["Julius Caesar", "Into the Abyss ", "Discobolous"];
-// const price = ["221", "157", "95"];
+const count = ["1", "2", "3"];
+const art = ["Julius Caesar", "Into the Abyss ", "Discobolous"];
+const price = ["221", "157", "95"];
 
-// var swiper = new Swiper(".main-swiper", {
-//   direction: "vertical",
-//   slidesPerView: 1,
-//   spaceBetween: 30,
-//   mousewheel: true,
-//   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true,
-//     renderBullet: function (index, className) {
-//       return (
-//         '<p class="' +
-//         className +
-//         '">' +
-//         "#0" +
-//         count[index] +
-//         "<span class='pagination-art'>" +
-//         art[index] +
-//         "</span>" +
-//         "<span class='pagination-price'>" +
-//         ("<span>" + price[index] + "</span>" + " " + "ETH") +
-//         "</span>" +
-//         "</p>"
-//       );
-//     },
-//   },
-// });
+var swiper = new Swiper(".main-swiper", {
+  direction: "vertical",
+  slidesPerView: 1,
+  spaceBetween: 30,
+  mousewheel: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return (
+        '<p class="' +
+        className +
+        '">' +
+        "#0" +
+        count[index] +
+        "<span class='pagination-art'>" +
+        art[index] +
+        "</span>" +
+        "<span class='pagination-price'>" +
+        ("<span>" + price[index] + "</span>" + " " + "ETH") +
+        "</span>" +
+        "</p>"
+      );
+    },
+  },
+});
