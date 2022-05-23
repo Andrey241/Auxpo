7f4e63069d94d02d8f4c81b424d108be29d0010f
Andrey241 <Andrey75.9626@gmail.com>
2022-05-23 15:33:11 +0300

    more adaptive

--- JavaScript/JavsScript.js
+++ JavaScript/JavsScript.js
@@ -1,8 +1,10 @@
 function start() {
   const viewport_Height = Math.max(document.documentElement.clientHeight);
   const viewport_Width = Math.max(document.documentElement.clientWidth);
-  console.log(viewport_Height);
-
+  if (viewport_Width <= 762) {
+    document.getElementById("Button").removeAttribute("disabled");
+    console.log(document.getElementById("Button"));
+  }
   if (viewport_Width <= 542 && viewport_Height <= 920) {
     console.log(1);
 
@@ -15,9 +17,7 @@ function start() {
     slideWidth.forEach((item) => {
       item.style.width = `${viewport_Width - 45}px`;
     });
-    console.log(slideWidth);
   } else if (viewport_Width <= 920 && viewport_Height <= 542) {
-    console.log(1);
     const footers = document.querySelectorAll(".swiper-footer");
     const swiperBodys = document.querySelectorAll(".swiper-body");
     for (let i = 0; i < footers.length; i++) {
@@ -27,8 +27,13 @@ function start() {
     slideWidth.forEach((item) => {
       item.style.width = `${viewport_Width - 45}px`;
     });
-    console.log(slideWidth);
   }
 }
 start();
 window.addEventListener("resize", start);
+
+const burger = document.querySelector(".content-sidebar__button");
+burger.addEventListener("click", function () {
+  document.querySelector("nav").classList.toggle("active");
+  body.classList.toggle("lock");
+});
