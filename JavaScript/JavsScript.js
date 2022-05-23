ec7f098756844b2bdc3c4ec8a1dece08ad6fccdd
Andrey241 <Andrey75.9626@gmail.com>
2022-05-23 13:56:01 +0300

    adaptive

--- JavaScript/JavsScript.js
+++ JavaScript/JavsScript.js
@@ -3,7 +3,7 @@ function start() {
   const viewport_Width = Math.max(document.documentElement.clientWidth);
   console.log(viewport_Height);
 
-  if (viewport_Width <= 416 && viewport_Height <= 920) {
+  if (viewport_Width <= 542 && viewport_Height <= 920) {
     console.log(1);
 
     const footers = document.querySelectorAll(".swiper-footer");
@@ -16,7 +16,19 @@ function start() {
       item.style.width = `${viewport_Width - 45}px`;
     });
     console.log(slideWidth);
+  } else if (viewport_Width <= 920 && viewport_Height <= 542) {
+    console.log(1);
+    const footers = document.querySelectorAll(".swiper-footer");
+    const swiperBodys = document.querySelectorAll(".swiper-body");
+    for (let i = 0; i < footers.length; i++) {
+      swiperBodys[i].append(footers[i]);
+    }
+    const slideWidth = document.querySelectorAll(".swiper-slide");
+    slideWidth.forEach((item) => {
+      item.style.width = `${viewport_Width - 45}px`;
+    });
+    console.log(slideWidth);
   }
 }
-
+start();
 window.addEventListener("resize", start);
